d995746e4169fc262148b2318d5d46407da230c8
Arunima <e.arastogi@gmail.com>
2019-07-11 12:26:19 +0530

    updating Strategy Design pattern basic

---
+++ Java_design_patterns/Strategy_design_pattern/Executor.java
@@ -0,0 +1,12 @@
+package Strategy_design_pattern;
+
+public class Executor {
+	public static void main(String[] args) {
+		Context context = new Context(new OperationAdd());
+		System.out.println("10 + 5 = " + context.executeStrategy(10, 5));
+
+		context = new Context(new OperationMultiply());
+		System.out.println("10 * 5 = " + context.executeStrategy(10, 5));
+
+	}
+}
---
+++ Java_design_patterns/Strategy_design_pattern/Strategy.java
@@ -0,0 +1,5 @@
+package Strategy_design_pattern;
+
+public interface Strategy {
+	public int doOperation(int num1, int num2);
+}
